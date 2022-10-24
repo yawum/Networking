28b583c5af7ed352eb3b639e6a4a3b1e96cceb35
yawum <107602549+yawum@users.noreply.github.com>
2022-10-23 20:30:52 -0400

    Add files via upload

---
+++ Powershell1.ps1
@@ -0,0 +1,26 @@
+﻿$numbers = Read-Host "What number would you want?"
+
+Do {
+$firstname = Read-Host " Enter firstname"
+$lastname = Read-Host "Enter lastname"
+$fullname = Read-Host " Enter fullname"
+$userprincipalname = Read-Host " Enter userprincipalname"
+$Samaccountname = Read-Host "Enter SamAccountname"
+$new=Read-Host "Enter the new password" -AsSecureString
+
+
+
+
+New-ADUser -Name $fullname -GivenName $firstname -Surname $lastname -UserPrincipalName $userprincipalname -SamAccountName $Samaccountname
+Set-ADAccountPassword $Samaccountname -NewPassword $new
+Enable-ADAccount $Samaccountname
+Get-ADUser $Samaccountname
+
+
+
+$numbers = $numbers - 1
+Write-Host "THE CURRENT VALUE OF THE VARIABLE IS $NUMBERS"
+
+
+
+} While ($NUMBERS -GT 0)
